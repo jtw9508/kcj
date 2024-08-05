c42ba3f74032f70a3c419879cb57d1de6fd8978b
donghyun-chae <danny001006@gmail.com>
2024-08-05 22:08:42 +0900

    add env

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, url_for, redirect
+from pymongo import MongoClient
+import random
+
 app = Flask(__name__)
 
+
+# LOAD .env FILE
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+CLIENT_ID = os.getenv("CLIENT_ID")
+
+client = MongoClient(CLIENT_ID)
+db = client.kcj
+
+# MAIN & READ CARD
 @app.route('/')
-def home():
-   return render_template('index.html')
+def index():
+    cards = list(db.cards.find({}))
+    card = random.choice(cards)
+    return render_template('index.html', card=card)
+
+# CREATE CARD
+# @app.route('/add', methods=['POST'])
+# def add():
+#     card = request.form['card']
+#     return redirect(url_for('index'))
+
 
 
 if __name__ == '__main__':
